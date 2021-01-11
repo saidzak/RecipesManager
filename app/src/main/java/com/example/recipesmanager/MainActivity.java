deffd76fc62319f4edf9874542dd577888a3ad8b
saidzak <saidzak1905@gmail.com>
2021-01-11 14:56:44 +0100

    Added class recette

--- app/src/main/java/com/example/recipesmanager/MainActivity.java
+++ app/src/main/java/com/example/recipesmanager/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    ArrayList<Recette> list = new ArrayList<Recette>();
+    Recette r = new Recette("Velouté de Potiron et Carottes", "déjeuner", 4 , "débutant", new String[] {"1 kg de potiron","500 g de carotte","2 pommes de terre","1 gousse d'ail","1 oignon","500 ml de lait","500 ml de bouillon de volaille","1 cuillère à soupe d'huile d'olive","Persil","Poivre", "Sel","Muscade", "10 cl de crème liquide (facultatif)"}, "Éplucher et couper en dés le potiron, les pommes de terre, les carottes en rondelles.\n" +
+            "Emincer l'ail et l'oignon.\n" +
+            "Faire suer l'oignon dans l'huile d'olive.\n" +
+            "Ajouter tous les légumes et l'ail puis verser le bouillon et le lait.\n" +
+            "Saler, poivrer, \"muscader\" et laisser cuire environ une trentaine de minutes.\n" +
+            "Mixer le tout (ajouter éventuellement la crème) et rectifier l'assaisonnement si nécessaire.\n" +
+            "Bon appétit !");
+
+    //Constructeur
+    protected MainActivity(){
+        list.add(r);
+    }
+
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/example/recipesmanager/Recette.java
@@ -0,0 +1,72 @@
+package com.example.recipesmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recette {
+
+    String titre;
+    String type;
+    int nbpersonnes;
+    String niveau;
+    String ingredients[];
+    String instruction;
+
+    public Recette(String titre, String type, int nbpersonnes, String niveau, String[] ingredients, String instruction) {
+        this.titre = titre;
+        this.type = type;
+        this.nbpersonnes = nbpersonnes;
+        this.niveau = niveau;
+        this.ingredients = ingredients;
+    }
+
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getNbpersonnes() {
+        return nbpersonnes;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setNbpersonnes(int nbpersonnes) {
+        this.nbpersonnes = nbpersonnes;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+}
